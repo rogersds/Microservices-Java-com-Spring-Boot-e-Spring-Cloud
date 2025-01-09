b198d40a65d246374ea627240513c61df8878796
rogersds <rogger_souza@hotmail.com>
2025-01-09 20:24:36 -0300

    Configuring Zuul API Gataway

---
+++ hr-api-gateway-zuul/src/test/java/com/devsuperior/Hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
