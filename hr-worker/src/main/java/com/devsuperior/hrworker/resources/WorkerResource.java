8cd6e9cb4cc0b17c84474cc62d1813464e220740
rogersds <rogger_souza@hotmail.com>
2025-01-12 09:19:12 -0300

    hr-worker as a config worker client

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +19,28 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
-	
-	//para imprimir informações 
+
+	// para imprimir informações
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
-	
-	
+
+	@Value("${test.config}")
+	private String testConfig;
 	@Autowired
 	private Environment env;
-	
-	
+
 	@Autowired // forma de fazer injeção de dependência de forma automática
 	private WorkerRepository repository;
+
+	
+	// teste 
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfig() {
+		logger.info("CONFIG = " + testConfig);
+		
+		return ResponseEntity.noContent().build();
+	}
+
+	
 	
 	// responseEntity encapsula uma resposta http na saida
 	@GetMapping
@@ -36,18 +48,18 @@ public class WorkerResource {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		
+
 		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
+
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-		
-		// para que o id passado na requisição seja reconehcido e cai na variavel @GetMapping(value = "/{id}"),
+
+		// para que o id passado na requisição seja reconehcido e cai na variavel
+		// @GetMapping(value = "/{id}"),
 		// usamos a anotation @Pathvariable
 	}
 
-
 }
-
