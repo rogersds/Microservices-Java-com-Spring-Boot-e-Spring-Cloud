da72d32d08f7b06b1d5e6da7293694dbeb624032
rogersds <rogger_souza@hotmail.com>
2025-01-12 10:26:44 -0300

    private Git Repository

--- hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/devsuperior/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.devsuperior.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -8,10 +10,25 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication implements CommandLineRunner  {
 
+	@Value("${spring.cloud.config.server.git.username}")
+	private String userName;
+	
+	@Value("${spring.cloud.config.server.git.password}")
+	private String password;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("USERNAME = " + userName);
+		System.out.println("USERNAME = " + password);
+
+		
 	}
 
 }
