df3adcab0252cf8725b8edbbdb4998e511564aa0
rogersds <rogger_souza@hotmail.com>
2025-01-22 19:35:51 -0300

    Security configs to Config server

--- hr-oauth/src/main/java/com/devsuperior/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.hroauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,23 +15,26 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationServerConfig extends  AuthorizationServerConfigurerAdapter{
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 	
+	@Value("${oauth.client.name}")
+	private String clientName;
+	
+	@Value("${oauth.client.secret}")
+	private String clientSecret;
 	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-
+	
 	@Autowired
 	private JwtAccessTokenConverter accessTokenConverter;
-
+	
 	@Autowired
 	private JwtTokenStore tokenStore;
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
-	
-
+		
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -39,8 +43,8 @@ public class AuthorizationServerConfig extends  AuthorizationServerConfigurerAda
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("myappname123")
-		.secret(passwordEncoder.encode("myappsecret123"))
+		.withClient(clientName)
+		.secret(passwordEncoder.encode(clientSecret))
 		.scopes("read", "write")
 		.authorizedGrantTypes("password")
 		.accessTokenValiditySeconds(86400);
@@ -52,6 +56,4 @@ public class AuthorizationServerConfig extends  AuthorizationServerConfigurerAda
 		.tokenStore(tokenStore)
 		.accessTokenConverter(accessTokenConverter);
 	}
-	
-
 }
